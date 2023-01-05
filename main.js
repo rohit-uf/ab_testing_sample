4ce2f56427a5b879c348206fb11c4df90818704d
rohit-uf <103091956+rohit-uf@users.noreply.github.com>
2023-01-05 18:38:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ const btn = document.querySelector('#btn');
 if(btn){
   btn.addEventListener('click', e=>{ alert('Hello World'); });
 }
+
+const div = document.querySelector('#container');
+let delta = -1;
+
+window.requestAnimationFrame(t=>{
+  if(div){
+      if(div.style.top == "0%"){
+          delta *= -1;
+      }
+      div.style.top = `${parseInt(div.style.top) + delta}%`;
+  }
+})
